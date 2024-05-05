f0d43a7726ec811b702179f76c087eca09ad691c
BakhodirovDev <bbahodirov.dev@gmail.com>
2024-05-05 18:17:32 +0500

    Add ProductsForm.cs

---
+++ Manager/ProductsForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TechnicalProductsStore.Manager
+{
+    partial class ProductsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            productBindingSource = new BindingSource(components);
+            idDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            productNameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            productCountryDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            productPriceDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            productDescriptionDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            productEnterCountDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            productEnterDataDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            remainingProductCountDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)productBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { idDataGridViewTextBoxColumn, productNameDataGridViewTextBoxColumn, productCountryDataGridViewTextBoxColumn, productPriceDataGridViewTextBoxColumn, productDescriptionDataGridViewTextBoxColumn, productEnterCountDataGridViewTextBoxColumn, productEnterDataDataGridViewTextBoxColumn, remainingProductCountDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = productBindingSource;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(1524, 450);
+            dataGridView1.TabIndex = 0;
+            // 
+            // productBindingSource
+            // 
+            productBindingSource.DataSource = typeof(Class.Product);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            idDataGridViewTextBoxColumn.HeaderText = "Id";
+            idDataGridViewTextBoxColumn.MinimumWidth = 8;
+            idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            idDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // productNameDataGridViewTextBoxColumn
+            // 
+            productNameDataGridViewTextBoxColumn.DataPropertyName = "ProductName";
+            productNameDataGridViewTextBoxColumn.HeaderText = "ProductName";
+            productNameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            productNameDataGridViewTextBoxColumn.Name = "productNameDataGridViewTextBoxColumn";
+            productNameDataGridViewTextBoxColumn.Width = 250;
+            // 
+            // productCountryDataGridViewTextBoxColumn
+            // 
+            productCountryDataGridViewTextBoxColumn.DataPropertyName = "ProductCountry";
+            productCountryDataGridViewTextBoxColumn.HeaderText = "ProductCountry";
+            productCountryDataGridViewTextBoxColumn.MinimumWidth = 8;
+            productCountryDataGridViewTextBoxColumn.Name = "productCountryDataGridViewTextBoxColumn";
+            productCountryDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // productPriceDataGridViewTextBoxColumn
+            // 
+            productPriceDataGridViewTextBoxColumn.DataPropertyName = "ProductPrice";
+            productPriceDataGridViewTextBoxColumn.HeaderText = "ProductPrice";
+            productPriceDataGridViewTextBoxColumn.MinimumWidth = 8;
+            productPriceDataGridViewTextBoxColumn.Name = "productPriceDataGridViewTextBoxColumn";
+            productPriceDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // productDescriptionDataGridViewTextBoxColumn
+            // 
+            productDescriptionDataGridViewTextBoxColumn.DataPropertyName = "ProductDescription";
+            productDescriptionDataGridViewTextBoxColumn.HeaderText = "ProductDescription";
+            productDescriptionDataGridViewTextBoxColumn.MinimumWidth = 8;
+            productDescriptionDataGridViewTextBoxColumn.Name = "productDescriptionDataGridViewTextBoxColumn";
+            productDescriptionDataGridViewTextBoxColumn.Width = 180;
+            // 
+            // productEnterCountDataGridViewTextBoxColumn
+            // 
+            productEnterCountDataGridViewTextBoxColumn.DataPropertyName = "ProductEnterCount";
+            productEnterCountDataGridViewTextBoxColumn.HeaderText = "ProductEnterCount";
+            productEnterCountDataGridViewTextBoxColumn.MinimumWidth = 8;
+            productEnterCountDataGridViewTextBoxColumn.Name = "productEnterCountDataGridViewTextBoxColumn";
+            productEnterCountDataGridViewTextBoxColumn.Width = 180;
+            // 
+            // productEnterDataDataGridViewTextBoxColumn
+            // 
+            productEnterDataDataGridViewTextBoxColumn.DataPropertyName = "ProductEnterData";
+            productEnterDataDataGridViewTextBoxColumn.HeaderText = "ProductEnterData";
+            productEnterDataDataGridViewTextBoxColumn.MinimumWidth = 8;
+            productEnterDataDataGridViewTextBoxColumn.Name = "productEnterDataDataGridViewTextBoxColumn";
+            productEnterDataDataGridViewTextBoxColumn.Width = 180;
+            // 
+            // remainingProductCountDataGridViewTextBoxColumn
+            // 
+            remainingProductCountDataGridViewTextBoxColumn.DataPropertyName = "RemainingProductCount";
+            remainingProductCountDataGridViewTextBoxColumn.HeaderText = "RemainingProductCount";
+            remainingProductCountDataGridViewTextBoxColumn.MinimumWidth = 8;
+            remainingProductCountDataGridViewTextBoxColumn.Name = "remainingProductCountDataGridViewTextBoxColumn";
+            remainingProductCountDataGridViewTextBoxColumn.Width = 220;
+            // 
+            // ProductsForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1524, 450);
+            Controls.Add(dataGridView1);
+            Name = "ProductsForm";
+            Text = "ProductsForm";
+            Load += ProductsForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)productBindingSource).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private BindingSource productBindingSource;
+        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn productNameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn productCountryDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn productPriceDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn productDescriptionDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn productEnterCountDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn productEnterDataDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn remainingProductCountDataGridViewTextBoxColumn;
+    }
+}
---
+++ Manager/ProductsForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TechnicalProductsStore.Class;
+using System.Text.Json;
+
+namespace TechnicalProductsStore.Manager
+{
+    public partial class ProductsForm : Form
+    {
+        List<Product> users = new List<Product>();
+        public ProductsForm()
+        {
+            InitializeComponent();
+            string path = @"../../../DataBase/Products.json";
+
+            if (File.Exists(path))
+            {
+                var existingUsersJson = File.ReadAllText(path);
+                users = JsonSerializer.Deserialize<List<Product>>(existingUsersJson);
+            }
+            dataGridView1.DataSource = users;
+
+
+            // Create ContextMenuStrip
+            ContextMenuStrip menuStrip = new ContextMenuStrip();
+            // Add "Edit", "Delete" and "Save" options
+            menuStrip.Items.Add("Edit");
+            menuStrip.Items.Add("Delete");
+            menuStrip.Items.Add("Save");
+            menuStrip.Items.Add("Create");
+            // Assign the ContextMenuStrip to the DataGridView
+            dataGridView1.ContextMenuStrip = menuStrip;
+
+            
+        }
+        
+
+        
+
+        
+
+        
+
+        private void ProductsForm_Load(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
