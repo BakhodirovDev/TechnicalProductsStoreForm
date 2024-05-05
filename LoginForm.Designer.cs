0519bc05d40ff5ad8615ee00799845ee95045635
Mirjahon Istamov <istamovmirjahon8@gmail.com>
2024-05-05 11:26:58 +0500

    Change LoginForm Style

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -39,9 +39,13 @@
             pictureBox2 = new PictureBox();
             panel1 = new Panel();
             btHideEyes = new Button();
+            panel2 = new Panel();
+            button1 = new Button();
+            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // btLogin
@@ -51,6 +55,7 @@
             btLogin.FlatAppearance.BorderColor = Color.Black;
             btLogin.FlatAppearance.MouseDownBackColor = Color.Green;
             btLogin.FlatAppearance.MouseOverBackColor = Color.Green;
+            btLogin.Font = new Font("Sylfaen", 10.2F, FontStyle.Bold);
             btLogin.Location = new Point(64, 244);
             btLogin.Name = "btLogin";
             btLogin.Size = new Size(178, 44);
@@ -62,9 +67,10 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(59, 57);
+            label1.Font = new Font("Sylfaen", 10.2F, FontStyle.Bold);
+            label1.Location = new Point(59, 69);
             label1.Name = "label1";
-            label1.Size = new Size(78, 20);
+            label1.Size = new Size(94, 22);
             label1.TabIndex = 1;
             label1.Text = "UserName";
             // 
@@ -79,9 +85,10 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(64, 147);
+            label2.Font = new Font("Sylfaen", 10.2F, FontStyle.Bold);
+            label2.Location = new Point(59, 159);
             label2.Name = "label2";
-            label2.Size = new Size(74, 20);
+            label2.Size = new Size(91, 22);
             label2.TabIndex = 3;
             label2.Text = "Password ";
             // 
@@ -89,24 +96,27 @@
             // 
             tbPassword.Location = new Point(59, 182);
             tbPassword.Name = "tbPassword";
+            tbPassword.PasswordChar = '*';
             tbPassword.Size = new Size(193, 27);
             tbPassword.TabIndex = 4;
             // 
             // label3
             // 
             label3.AutoSize = true;
+            label3.Font = new Font("Sylfaen", 10.2F, FontStyle.Bold);
+            label3.ForeColor = SystemColors.ButtonHighlight;
             label3.Location = new Point(133, 19);
             label3.Name = "label3";
-            label3.Size = new Size(46, 20);
+            label3.Size = new Size(56, 22);
             label3.TabIndex = 5;
             label3.Text = "Login";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(21, 86);
+            pictureBox1.Location = new Point(21, 94);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(32, 35);
+            pictureBox1.Size = new Size(32, 27);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 6;
             pictureBox1.TabStop = false;
@@ -114,9 +124,9 @@
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(21, 176);
+            pictureBox2.Location = new Point(21, 182);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(32, 35);
+            pictureBox2.Size = new Size(32, 29);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 7;
             pictureBox2.TabStop = false;
@@ -134,16 +144,51 @@
             panel1.Controls.Add(tbUserName);
             panel1.Location = new Point(43, 96);
             panel1.Name = "panel1";
-            panel1.Size = new Size(321, 303);
+            panel1.Size = new Size(320, 315);
             panel1.TabIndex = 8;
             // 
             // btHideEyes
             // 
+            btHideEyes.BackgroundImage = (Image)resources.GetObject("btHideEyes.BackgroundImage");
+            btHideEyes.BackgroundImageLayout = ImageLayout.Stretch;
             btHideEyes.Location = new Point(258, 182);
             btHideEyes.Name = "btHideEyes";
             btHideEyes.Size = new Size(34, 29);
             btHideEyes.TabIndex = 8;
             btHideEyes.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.BackgroundImageLayout = ImageLayout.None;
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(button1);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(899, 35);
+            panel2.TabIndex = 9;
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.Location = new Point(862, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(34, 29);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Sylfaen", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(3, 7);
+            label4.Name = "label4";
+            label4.Size = new Size(218, 22);
+            label4.TabIndex = 1;
+            label4.Text = "Technical Products Store";
             // 
             // LoginForm
             // 
@@ -152,10 +197,12 @@
             BackColor = Color.SteelBlue;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Zoom;
-            ClientSize = new Size(860, 457);
+            ClientSize = new Size(899, 499);
+            Controls.Add(panel2);
             Controls.Add(panel1);
             DoubleBuffered = true;
             ForeColor = SystemColors.ActiveCaptionText;
+            FormBorderStyle = FormBorderStyle.None;
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterParent;
             Load += LoginForm_Load;
@@ -163,6 +210,8 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -178,5 +227,8 @@
         private PictureBox pictureBox2;
         private Panel panel1;
         private Button btHideEyes;
+        private Panel panel2;
+        private Button button1;
+        private Label label4;
     }
 }
--- LoginForm.cs
+++ LoginForm.cs
@@ -29,39 +29,44 @@ namespace TechnicalProductsStore
         //test git folders
         private void btLogin_Click(object sender, EventArgs e)
         {
-            if(string.IsNullOrEmpty(tbUserName.Text) || string.IsNullOrEmpty(tbPassword.Text)) 
+            if (string.IsNullOrEmpty(tbUserName.Text) || string.IsNullOrEmpty(tbPassword.Text))
             {
-                MessageBox.Show("Ma'lumotlarni to'liq kiriting.Qayta urining","Information",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                MessageBox.Show("Ma'lumotlarni to'liq kiriting.Qayta urining", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
 
             string pathUserList = @"../../../DataBase/Users.json";
             List<Users> userList = new List<Users>();
 
-            if(File.Exists(pathUserList))
+            if (File.Exists(pathUserList))
             {
                 string jsonFormat = File.ReadAllText(pathUserList);
                 userList = JsonSerializer.Deserialize<List<Users>>(jsonFormat);
             }
-            if(userList.Any(item=>item.UserName == tbUserName.Text && item.Password == tbPassword.Text))
+            if (userList.Any(item => item.UserName == tbUserName.Text && item.Password == tbPassword.Text))
             {
-                if(userList.Any(m=>m.Role.ToLower() == "manager"))
+                if (userList.Any(m => m.Role.ToLower() == "manager"))
                 {
                     this.Hide();
                     ManagerForm form = new ManagerForm();
                     form.StartPosition = FormStartPosition.CenterParent;
                     form.Show();
-                    
+
                 }
                 else
                 {
-                    this.Hide();
+                    // this.Hide();
 
                 }
             }
             else
             {
-                MessageBox.Show("Bunaqa foydalanuvchi topilmadi.Try again","ERROR",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                MessageBox.Show("Bunaqa foydalanuvchi topilmadi.Try again", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
