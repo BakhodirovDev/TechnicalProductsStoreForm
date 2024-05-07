82c04ade6920a15f8aa7fc8d4ada285de4fb0967
BakhodirovDev <bbahodirov.dev@gmail.com>
2024-05-07 18:19:46 +0500

    Change Bugs

--- LoginForm.cs
+++ LoginForm.cs
@@ -66,7 +66,7 @@ namespace TechnicalProductsStore
                 {
                     
                     this.Hide();
-                    SellerForm sellerForm = new SellerForm();
+                    SellerForm sellerForm = new SellerForm(user.ID);
                     sellerForm.StartPosition = FormStartPosition.CenterScreen;
                     sellerForm.Show();
 
